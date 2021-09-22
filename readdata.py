c23a44c6ed54ff568b3bd0acfdeb6a999171fa25
Colin Caprani <colin.caprani@monash.edu>
2021-09-22 11:34:13 +1000

    initial

---
+++ readdata.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+
+"""
+Contains data from the Monash Unviersity Woodside Living Lab Building 
+recording of the 22 Sept 2021 Mansfield 5.8M earthquake
+"""
+
+from nptdms import TdmsFile
+import matplotlib.pyplot as plt
+
+
+tdms_file = TdmsFile.read("202109220920_SHM-6.tdms")
+group = tdms_file.groups()[0]
+channels = group.channels()
+
+fig, ax = plt.subplots()
+
+plot_list = range(len(channels))
+for c in plot_list:
+    channel = channels[c]
+    data = channel[:]
+    t = channel.time_track(absolute_time=True)
+
+    ax.plot(t, data, label=channel.name)
+
+ax.legend()
+plt.show()
